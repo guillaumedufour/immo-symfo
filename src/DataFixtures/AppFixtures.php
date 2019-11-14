de488274e9631d008bbc345045b749d87909c4fb
Guillaume Dufour <dufour.g@live.fr>
2019-11-14 15:36:58 +0100

    modif fixture, ajout adcontroller,entite image

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -28,6 +29,15 @@ class AppFixtures extends Fixture
                 ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
                 ->setRooms(mt_rand(1, 6));
+
+            for ($j = 1; $j <= mt_rand(2, 5); $j++) {
+                $image = new Image();
+
+                $image->setUrl($faker->imageUrl())
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+                $manager->persist($image);
+            }
 
             $manager->persist($ad);
         }
