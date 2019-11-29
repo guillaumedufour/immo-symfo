de751b320d5948ca89b7e86d419a65921e8a1044
Guillaume Dufour <dufour.g@live.fr>
2019-11-29 13:52:59 +0100

    user management beginning

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,8 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
-
 use App\Entity\Image;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -15,6 +15,39 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('FR-fr');
 
+        //users management
+        $users = [];
+        $genres = ['male', 'female'];
+
+        for ($i = 1; $i <= 10; $i++) {
+            $user = new User();
+
+            $genre = $faker->randomElement($genres);
+            $picture = 'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1, 99).'.jpg';
+
+            if ($genre === 'male') {
+                $picture = $picture.'men/'.$pictureId;
+            } else {
+                $picture = $picture.'women/'.$pictureId;
+            }
+
+            $user
+                ->setFirstname($faker->firstName($genre))
+                ->setLastname($faker->lastName)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'.implode('</p><p>', $faker->paragraphs(2)).'</p>')
+                ->setHash('password')
+                ->setPicture($picture);
+
+            $manager->persist($user);
+            $users[] = $user;
+
+
+        }
+
+        //ads management
         for ($i = 1; $i <= 30; $i++) {
             $ad = new Ad();
             $title = $faker->sentence();
@@ -22,18 +55,22 @@ class AppFixtures extends Fixture
             $coverImage = $faker->imageUrl(1920, 1080);
             $introduction = $faker->paragraph(2);
             $content = '<p>'.implode('</p><p>', $faker->paragraphs(5)).'</p>';
+            $user = $users[mt_rand(0, count($users) - 1)];
 
-            $ad->setTitle($title)
+            $ad
+                ->setTitle($title)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
-                ->setRooms(mt_rand(1, 6));
+                ->setRooms(mt_rand(1, 6))
+                ->setAuthor($user);
 
             for ($j = 1; $j <= mt_rand(2, 5); $j++) {
                 $image = new Image();
 
-                $image->setUrl($faker->imageUrl())
+                $image
+                    ->setUrl($faker->imageUrl())
                     ->setCaption($faker->sentence())
                     ->setAd($ad);
                 $manager->persist($image);
