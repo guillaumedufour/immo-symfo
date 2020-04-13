1ad90f88cca5cadd310632c6aa53e03422d48c85
Guillaume Dufour <dufour.g@live.fr>
2020-04-13 17:34:16 +0200

    my bookings

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -139,4 +139,15 @@ class AccountController extends AbstractController
             'user' => $this->getUser()
         ]);
     }
+
+    /**
+     * display user's bookings
+     * @Route("/account/bookings",name="account_bookings")
+     * @IsGranted("ROLE_USER")
+     * @return Response
+     */
+    public function bookings()
+    {
+        return $this->render('account/bookings.html.twig');
+    }
 }
